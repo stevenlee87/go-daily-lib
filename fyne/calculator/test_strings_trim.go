373e89c703e7daaddffcc9eb14c4f8ec50e0d747
stevenlee87 <imepisode1@gmail.com>
2021-01-05 19:16:07 +0800

    add new code of strings

--- fyne/calculator/test_strings_trim.go
+++ fyne/calculator/test_strings_trim.go
@@ -6,7 +6,15 @@ import (
 )
 
 func main() {
-	line := "+"
-	line = strings.Trim(line, "+÷×")
+	line := "123"
+	line = strings.Trim(line, "23")
 	fmt.Println(line)
+
+	line2 := "1+2"
+	fmt.Printf("line2 is %s\n", line2)
+	line2 = strings.Trim(line, "+÷×")
+	fmt.Println(line2)
+
+	fmt.Print(strings.Trim("¡¡¡Hello, Gophers!!!", "!¡"))
+
 }
