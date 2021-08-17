eef55a0676d18089dc0cb96f2415112025ab5243
stevenlee87 <imepisode1@gmail.com>
2021-08-17 14:47:28 +0800

    add new code:golang resume example

---
+++ Golang_常见面试题目解析/example10_stuct.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type People struct {
+	Name string
+}
+
+func (p *People) String() string {
+	return fmt.Sprintf("print: %v", p)
+}
+
+func main() {
+	p := &People{}
+	p.String()
+}
+
+/*
+在golang中 String() string ⽅法实际上是实现了 String 的接⼝的，该接⼝定义在
+fmt/print.go 中：
+type Stringer interface {
+	String() string
+}
+
+在使⽤  fmt 包中的打印⽅法时，如果类型实现了这个接⼝，会直接调⽤。⽽题⽬中打
+印 p 的时候会直接调⽤ p 实现的  String() ⽅法，然后就产⽣了循环调⽤。
+*/
---
+++ Golang_常见面试题目解析/example11_channel2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int, 1000)
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+
+	go func() {
+		for {
+			a, ok := <-ch
+			if !ok {
+				fmt.Println("close")
+				return
+			}
+			fmt.Println("a: ", a)
+		}
+	}()
+	time.Sleep(time.Second * 3)
+	fmt.Println("ok")
+	close(ch)
+}
+
+/*
+解析：
+在 golang 中  goroutine 的调度时间是不确定的，
+在题⽬中，第⼀个写 channel 的 goroutine 可能还未调⽤，或已调⽤但没有写完时直接 close 管道，
+可能导致写失败，即出现 panic 错误。
+*/
