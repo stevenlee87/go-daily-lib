81ba45387e797a81062b8deb8e10249aacda681c
stevenlee87 <imepisode1@gmail.com>
2021-09-01 14:44:33 +0800

    add new code 16 multi reference and channel:golang resume example

--- Golang_常见面试题目解析/16_multi_reference/example16_multi_reference.go
+++ Golang_常见面试题目解析/16_multi_reference/example16_multi_reference.go
@@ -4,9 +4,11 @@ import "fmt"
 
 type query func(string) string
 
-func exec(name string, vs ...query) string { // var vs []query fslice := []int(arg)
+func exec(name string, vs ...query) string { // var vs []query
+	//fmt.Println("vs --->", vs)
 	ch := make(chan string)
 	fn := func(i int) {
+		fmt.Println("vs[i](name) --->", vs[i](name))
 		ch <- vs[i](name) // vs[0]("111")
 	}
 
@@ -28,9 +30,13 @@ func main() {
 		return n + "func4"
 	})
 	fmt.Println(ret)
+
 }
 
 /*
 依据4个goroutine的启动后执⾏效率，很可能打印111func4，但其他的111func*也
 可能先执⾏，exec只会返回⼀条信息。
+
+上⾯的代码有严重的内存泄漏问题，出错的位置是 go fn(i) ，实际上代码执⾏后会启动4个协程，
+但是因为 ch 是⾮缓冲的，只可能有⼀个协程写⼊成功。⽽其他三个协程会⼀直在后台等待写⼊。
 */
