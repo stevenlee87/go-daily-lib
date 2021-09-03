d5eeab56c9c4f82a36908c7b4e19d66348f0d3e0
stevenlee87 <imepisode1@gmail.com>
2021-09-03 10:27:48 +0800

    add new code 02 strings.Count:golang resume example

---
+++ golang_resume_exercise/02_strings_Count/go.mod
@@ -0,0 +1,5 @@
+module github.com/stevenlee87/go-daily-lib/tree/main/golang_resume_exercise/02_strings_Count
+
+go 1.16
+
+require github.com/mingrammer/commonregex v1.0.1
--- golang_resume_exercise/02_strings_Count/test_mac_md5.go
+++ golang_resume_exercise/02_strings_Count/test_mac_md5.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	//"strings"
 
 	"github.com/mingrammer/commonregex"
 )
 
 func main() {
-	text := `mac address: ac:de:48:00:11:22, ip: 192.168.3.20, md5: fdbf72fdabb67ea6ef7ff5155a44def4`
+	text := `mac address: ac:de:48:00:11:22, ip: 192.168.3.20, md5: fdbf72fdabb67ea6ef7ff5155a44def4 192.168.1.100 192.168.1.100`
 
 	macList := commonregex.MACAddresses(text)
 	ipList := commonregex.IPs(text)
@@ -16,4 +19,12 @@ func main() {
 	fmt.Println("mac list:", macList)
 	fmt.Println("ip list:", ipList)
 	fmt.Println("md5 list:", md5List)
+
+	for _, v := range ipList {
+		//fmt.Println(v)
+		if strings.Count(string(ipList), v) > 1 {
+			fmt.Println(v)
+		}
+	}
+
 }
